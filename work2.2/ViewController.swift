792dcb95b2a5b4a2a39fba5214d3dc4209667c10
L.A <alexey.lit88@gmail.com>
2024-10-31 23:27:07 +0300

    Initial Commit

---
+++ work2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  work2.2
+//
+//  Created by ALEXEY LITVINOV on 31.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
